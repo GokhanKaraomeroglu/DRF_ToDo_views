01fda9fe258ef114fa7011ebaf2811b90e0429c0
Himmel König <itogreniyorum@gmail.com>
2021-11-08 21:35:10 +0300

    view generated

--- apiTodo/views.py
+++ apiTodo/views.py
@@ -1,7 +1,23 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .serializers import TodoSerializer
+from .models import Todo
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 def home (request):
   return HttpResponse('<center><h1 style="background-color:powderblue;">KHAN DRF Funtional View Page</h1><center>')
+
+
+@api_view()
+def TodoList(request):
+  querset = Todo.objects.all()
+  
+  serializer = TodoSerializer(querset, many=True)
+  
+  return Response (serializer.data)
+  
+  
