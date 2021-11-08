ac956a197d781e0f979b200c4395a06569fef376
Himmel König <itogreniyorum@gmail.com>
2021-11-08 21:01:46 +0300

    view and urls generated

--- apiTodo/views.py
+++ apiTodo/views.py
@@ -1,3 +1,7 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+  return HttpResponse('<center><h1 style="background-color:powderblue;">KHAN DRF Funtional View Page</h1><center>')
